6e4bed869fd728942e45fd74a2f5d8e50e7598d3
Minjoo Kim <mkim9313@uw.edu>
2022-06-07 14:20:05 -0700

    completed final project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+// persona image modal
+$('img#persona').css("cursor", "pointer");
+
+$('img#persona').attr('data-bs-toggle', 'modal');
+$('img#persona').attr('data-bs-target', '#myModal');
+
 $('#myModal').on("show.bs.modal", function(event){
     var imageClicked = $(event.relatedTarget);
     var srcClicked = imageClicked.attr("src");
@@ -5,3 +11,13 @@ $('#myModal').on("show.bs.modal", function(event){
     $("#modalImage").attr("src", srcClicked);
     $("#modalImage").attr("alt", altClicked);
 });
+
+//progress bar
+window.onscroll = function() {myFunction()};
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
